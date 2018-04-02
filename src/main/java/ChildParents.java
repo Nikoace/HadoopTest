be7b35c3cc3cd1f48b7c4a7006addfac2a4a123f
Acex <349538686@qq.com>
2018-04-02 20:32:05 +0800

    Initial commit

---
+++ src/main/java/ChildParents.java
@@ -0,0 +1,73 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ChildParents{
+    public static int time = 0;
+    public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+        protected void map(LongWritable key, Text value, Context context)
+                throws java.io.IOException, InterruptedException {
+            // 左右表的标识
+            int relation;
+            StringTokenizer tokenizer = new StringTokenizer(value.toString());
+            String child = tokenizer.nextToken();
+            String parent = tokenizer.nextToken();
+            if (child.compareTo("C") != 0) {
+                // 左表
+                relation = 1;
+                context.write(new Text(parent),
+                        new Text(relation + "+" + child));
+                // 右表
+                relation = 2;
+                context.write(new Text(child),
+                        new Text(relation + "+" + parent));
+            }
+        };
+
+    }
+    public static class reduce extends Reducer<Text, Text, Text, Text> {
+        protected void reduce(Text key, Iterable<Text> values,
+                              Reducer<Text, Text, Text, Text>.Context output)
+                throws java.io.IOException, InterruptedException {
+            int grandchildnum = 0;
+            int grandparentnum = 0;
+            List<String> grandchilds = new ArrayList<String> ();
+            List<String> grandparents = new ArrayList<String>();
+
+            /* 输出表头 */
+            if (time == 0) {
+                output.write(new Text("grandchild"), new Text("grandparent"));
+                time++;
+            }
+            for (Text val : values) {
+                String record = val.toString();
+                char relation = record.charAt(0);
+                // 取出此时key所对应的child
+                if (relation == '1') {
+                    String child = record.substring(2);
+                    grandchilds.add(child);
+                    grandchildnum++;
+                }
+                // 取出此时key所对应的parent
+                else if (relation == '2'){
+                    String parent = record.substring(2);
+                    grandparents.add(parent);
+                    grandparentnum++;
+                }
+            }
+            if (grandchildnum != 0 && grandparentnum != 0) {
+                for (int i = 0; i < grandchildnum; i++)
+                    for (int j = 0; j < grandparentnum; j++)
+                        output.write(new Text(grandchilds.get(i)), new Text(
+                                grandparents.get(j)));
+            }
+
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        /*删除之前生成的OutPut文件夹*/
+        FileSystem hdfs= FileSystem.get(conf);
+        Path del = new Path ( "/output" );
+        boolean isDel = hdfs.delete ( del,true );
+        /*验证hdfs路径（至少有两个）*/
+        String[] otherArgs = new GenericOptionsParser (conf, args).getRemainingArgs();
+        if (otherArgs.length < 2) {
+            System.err.println("Usage: ChildParents <in> [<in>...] <out>");
+            System.exit(2);
+        }
+        /*执行类*/
+        Job job = Job.getInstance(conf, "ChildParents");
+        job.setJarByClass(ChildParents.class);
+        /*进行Mapreduce时使用*/
+        job.setMapperClass(ChildParents.Map.class);
+        job.setReducerClass(ChildParents.reduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        for (int i = 0; i < otherArgs.length - 1; ++i) {
+            FileInputFormat.addInputPath(job, new Path (otherArgs[i]));
+        }
+        FileOutputFormat.setOutputPath(job,
+                new Path(otherArgs[otherArgs.length - 1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
